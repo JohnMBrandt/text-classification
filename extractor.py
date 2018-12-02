27538cef0f9d59b5621ba02c59fa9e277dc74fdb
John Brandt <john.brandt@yale.edu>
2018-12-02 18:01:16 -0500

    functionalize data loading and encoding

--- extractor.py
+++ extractor.py
@@ -24,21 +24,75 @@ base_dir = os.getcwd()
 dir_x = os.path.join(base_dir, "ndc-extraction", "x")
 dir_y = os.path.join(base_dir, "ndc-extraction", "y")
 glove_dir = os.path.join(base_dir, "glove-embeddings")
-ls_x = os.listdir(dir_x)
-ls_y = os.listdir(dir_y)
 
-ls_x = [x for x in ls_x if ".txt" in x]
-ls_y = [x for x in ls_y if ".txt" in x]
+def encode_sentences(weights):
+    dir_y = os.listdir(os.path.join(base_dir, "ndc-extraction", "y"))
+    dir_y = [x for x in dir_y if ".txt" in x]
+    file = os.path.join(base_dir, "obj/merge/sdg_indexes.txt")
+    classes = []
+    html_i = []
+    files = []
+    f = open(file)
+    for line in f:
+        l = line.strip().split(" ")
+        classes.append(l[0])
+        html_i.append(int(l[1]) - (int(l[1])//100)*100)
+        files.append(l[2])
+    f.close()
+    sent_encodings = np.array((len(sdg), 100))
+    for i, val in enumerate(sdg):
+        dim_1 = ls_y.index(files[i])
+        dim_2 = html_i[i]
+        current_encoding = sent_enc[dim_1, dim_2,]
+        sent_encodings[i] = current_encoding
+    return(sent_encodings)
 
-data_x = []
-data_y = []
 
+def load_data(dir_x, dir_y):
+    base_dir = os.getcwd()
+    ls_x = [x for x in os.listdir(dir_x) if ".txt" in x]
+    ls_y = [x for x in os.listdir(dir_y) if ".txt" in x]
+    data_x = []
+    data_y = []
 
-def to_one_hot(labels, dim=2):
-    results = np.zeros((len(labels), dim))
-    for i, label in enumerate(labels):
-        results[i][label - 1] = 1
-    return results
+    print("\n### Loading data ###")
+    for file in ls_x:
+        f = open(os.path.join(dir_x, file))
+        temp = []
+        for line in f:
+            temp.append(line.strip())
+        f.close()
+        data_x.append(temp)
+        
+    for file in ls_y:
+        f = open(os.path.join(dir_y, file))
+        temp = []
+        for line in f:
+            temp.append(line.strip())
+        f.close()
+        data_y.append(temp)
+
+    print("### Generating tokenizer ###")
+    tokenizer = Tokenizer(num_words = 10000)
+    tokenizer.fit_on_texts(data_x)
+    word_index = tokenizer.word_index
+
+    seq_x = []
+    for i in data_x:
+        if len(i) < 100:
+            i[len(i):100] = [''] * (100 - len(i))
+        temp = (tokenizer.texts_to_sequences(i))
+        temp = pad_sequences(temp, 30, value = 0)
+        seq_x.append(temp)
+        
+    binary_y = []
+    for i in data_y:
+        if len(i) < 100:
+            i[len(i):100] = [0] * (100 - len(i))
+        temp = [int(x) for x in i]
+        #temp = to_one_hot(temp)
+        binary_y.append(temp)
+    return(seq_x, binary_y, tokenizer)
 
 
 def load_embeddings(glove_dir, word_index, max_words, embedding_dim):
@@ -61,53 +115,6 @@ def load_embeddings(glove_dir, word_index, max_words, embedding_dim):
                 embedding_matrix[i] = embedding_vector
     return(embedding_matrix)
 
-print("\n### Loading data ###")
-for file in ls_x:
-    f = open(os.path.join(dir_x, file))
-    temp = []
-    for line in f:
-        temp.append(line.strip())
-    f.close()
-    data_x.append(temp)
-    
-for file in ls_y:
-    f = open(os.path.join(dir_y, file))
-    temp = []
-    for line in f:
-        temp.append(line.strip())
-    f.close()
-    data_y.append(temp)
-
-print("### Generating tokenizer ###")
-tokenizer = Tokenizer(num_words = 10000)
-tokenizer.fit_on_texts(data_x)
-word_index = tokenizer.word_index
-
-seq_x = []
-for i in data_x:
-    if len(i) < 100:
-        i[len(i):100] = [''] * (100 - len(i))
-    temp = (tokenizer.texts_to_sequences(i))
-    temp = pad_sequences(temp, 30)
-    seq_x.append(temp)
-    
-binary_y = []
-for i in data_y:
-    if len(i) < 100:
-        i[len(i):100] = [0] * (100 - len(i))
-    temp = [int(x) for x in i]
-    #temp = to_one_hot(temp)
-    binary_y.append(temp)
-
-train_x, train_y, validation_x, validation_y, test_x, test_y = split_train(training_samples = 300,
-                                                                            validation_samples = 1,
-                                                                            test_samples = 50,
-                                                                            shuffle = False,
-                                                                            x = seq_x,
-                                                                            y = binary_y)
-
-embedding_weights = load_embeddings(glove_dir, word_index, 10000, 300)
-
 class HierarchicalAttn():
     'Constructs a model with attention over words and Bidirectional GRU over sentences to encode sentences'
     def __init__(self, model, max_len, max_sentence, vocab_size, word_embedding, embedding_weights):
@@ -127,7 +134,7 @@ class HierarchicalAttn():
                 if val == 0:
                     indiv_wt[i] = 1
                 else:
-                    indiv_wt[i] = 5.6
+                    indiv_wt[i] = 12
             samp_wt[x] = indiv_wt
         return(samp_wt)
         
@@ -135,8 +142,9 @@ class HierarchicalAttn():
         'Build bi-level bi-directional GRU model with attention over word embeddings'
         l2_reg = regularizers.l2(1e-8)
         sentence_in = Input(shape = (self.max_len,), dtype = "int32")
-        embedded_word_seq = Embedding(10000, 300, input_length = self.max_len, trainable = False, weights = [self.embedding_weights])(sentence_in)
-        word_encoder = Bidirectional(GRU(50, return_sequences = True, kernel_regularizer = l2_reg, dropout = 0.2, recurrent_dropout = 0.2))(embedded_word_seq)
+        masking_layer = Masking(mask_value = 0)(sentence_in)
+        embedded_word_seq = Embedding(10000, 300, input_length = self.max_len, trainable = False, weights = [self.embedding_weights])(masking_layer)
+        word_encoder = Bidirectional(GRU(50, return_sequences = True, kernel_regularizer = l2_reg))(embedded_word_seq)
         dense_transform_w = Dense(100, activation = "relu", name = "dense_transform_w", kernel_regularizer = l2_reg)(word_encoder)
         attn_weighted_sent = Model(sentence_in, Attention(name = 'word_attention', regularizer = l2_reg)(dense_transform_w))
         attn_weighted_sent.summary()
@@ -174,21 +182,33 @@ class HierarchicalAttn():
         encoded_test_x = self.encode_texts(test_x)
         encoded_test_y = self.encode_y(test_y)
         encoded_train_y = self.encode_y(train_y)
-        encoded_all = self.encode_texts(seq_x)
+        encoded_all = self.encode_texts(data_x)
         sample_weights = self.weight_samples(train_y)
-        
+        csv_logger = CSVLogger("log_encoder.csv", append = False, separator = ",")
         self.model = self.build_model()
-        self.model.fit(encoded_train_x, encoded_train_y, epochs = 10, validation_split = 0.1, batch_size = 10,
-                        sample_weight = sample_weights)
+        self.model.fit(encoded_train_x, encoded_train_y, epochs = 5, validation_split = 0.25, batch_size = 10,
+                        sample_weight = sample_weights,  callbacks = [csv_logger])
         preds = self.model.predict(encoded_test_x)
+        return(model)
 
         # Extract second LSTM output for the sentence representation to be transfered to sentence classifier
         intermediate_layer_model = Model(inputs=self.model.input,
                                          outputs=self.model.get_layer("sentence_encoder").output)
         intermediate_output = intermediate_layer_model.predict(encoded_all)
-        save_obj(intermediate_output, "sentence_encoding")
+        encoded_sentences = encode_sentences(intermediate_output)
+        print(encoded_sentences.shape)
+        save_obj(encoded_sentences, "sentence_encoding")
         save_obj(preds, "sentence_extraction")
 
 if __name__ == "__main__":
+    data_x, data_y, tokenizer = load_data(dir_x, dir_y)
+    train_x, train_y, validation_x, validation_y, test_x, test_y = split_train(training_samples = 430,
+                                                                            validation_samples = 1,
+                                                                            test_samples = 10,
+                                                                            shuffle = False,
+                                                                            x = data_x,
+                                                                            y = data_y)
+
+    embedding_weights = load_embeddings(glove_dir, tokenizer.word_index, 10000, 300)
     h = HierarchicalAttn(None, 30, 500, 10000, 300, embedding_weights)
     h.train(train_x, train_y)
